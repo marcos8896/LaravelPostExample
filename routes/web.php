5ba53d398c76dfa7a71c9d642b8cf65ab0848341
marcos8896 <elyomarcos@gmail.com>
2017-04-14 20:31:13 -0600

    Lesson 24 - The Service Container - Completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+App::bind('App\Billing\Stripe', function() {
+  return new \App\Billing\Stripe(config('services.stripe.secret'));
+});
+
+//The following  three lines do the same thing.
+//$stripe = App::make('App\Billing\Stripe');
+//$stripe = app('App\Billing\Stripe');
+$stripe = resolve('App\Billing\Stripe');
+
+dd($stripe);
+
 Route::get('/', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
